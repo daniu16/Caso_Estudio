347fd8a9a1e2fdb274349860f3e992036e26448c
daniu16 <cesar2224642@correo.uis.edu.co>
2023-05-26 14:27:36 -0500

    parteconsolaterminada

--- controlador/Controlador.java
+++ controlador/Controlador.java
@@ -2,9 +2,8 @@ package controlador;
 
 import javax.swing.JOptionPane;
 import modelo.Nom;
-import modelo.Añ;
 import modelo.Aut;
-import modelo.Col;
+import modelo.Añ;
 import modelo.Edicc;
 import modelo.Edicclib;
 import modelo.LibCol;
@@ -15,33 +14,46 @@ public class Controlador {
 
         String confirmacion = "si";
 
-        while(confirmacion.equals("si")) {
+        while (confirmacion.equals("si")) {
             // Entrada de datos
             String Nombre = JOptionPane.showInputDialog("Ingrese el nombre del libro: ");
             String Autor = JOptionPane.showInputDialog("Ingrese el nombre del autor del libro: ");
             String Año = JOptionPane.showInputDialog("Ingrese el año de ingreso del libro: ");
-            String Edicion = JOptionPane.showInputDialog("El libro es de una edición de lujo; (si / no): ");
-            if(Edicion.equals("si")){
-                String Ediciondellibro = JOptionPane.showInputDialog("¿Cual es la ediccion del libro?: ");
-            }else{
-            String Ediciondellibro = JOptionPane.showInputDialog(null);
+            String Edicion = JOptionPane.showInputDialog("El libro es de una edición de lujo (si / no): ");
+            String Ediciondellibro;
+            if (Edicion.equals("si")) {
+                Ediciondellibro = JOptionPane.showInputDialog("El libro es de una edición de lujo: ");
+            } else {
+                Ediciondellibro = "El libro no es una edición de lujo";
             }
-            String Coleccion = JOptionPane.showInputDialog("El libro tiene una coleccion; (si / no): ");
-            if(Coleccion.equals("si")){
-                String Colecciondellibro = JOptionPane.showInputDialog("¿Cual es el numero de collecion del libro?: ");
-            }else{
-            String EColecciondellibro = JOptionPane.showInputDialog(null);
+            String Coleccion = JOptionPane.showInputDialog("El libro tiene una colección (si / no): ");
+            String Colecciondellibro;
+            if (Coleccion.equals("si")) {
+                Colecciondellibro = JOptionPane.showInputDialog("¿Cuál es el número de colección del libro?: ");
+            } else {
+                Colecciondellibro = "";
             }
 
             Nom miNombre = new Nom(Nombre);
-            miNombre.getResultadosNombre();
-            JOptionPane.showMessageDialog(null, "El nombre del libro es: " + miNombre.getResultadosNombre());
+            String resultadoNombre = miNombre.getResultadosNombre();
+            JOptionPane.showMessageDialog(null, "El nombre del libro es: " + resultadoNombre);
 
             Aut miAutor = new Aut(Autor);
-            miAutor.getResultadosAutor();
-            JOptionPane.showMessageDialog(null,"El autor o autores del libro es:" + miAutor.getResultadosAutor());
+            String resultadoAutor = miAutor.getResultadosAutor();
+            JOptionPane.showMessageDialog(null, "El autor o autores del libro son: " + resultadoAutor);
 
-            
+            Añ miAño = new Añ(Año);
+            String resultadoAño = miAño.getResultadosAño();
+            JOptionPane.showMessageDialog(null, "El año de edición del libro es: " + resultadoAño);
+
+            Edicclib miEdicclib = new Edicclib(Ediciondellibro);
+            String resultadoEdicion = miEdicclib.getResultadoEdicciondellibro();
+            JOptionPane.showMessageDialog(null, "La edición del libro es: " + resultadoEdicion);
+
+            LibCol miLibCol = new LibCol(Colecciondellibro);
+            String resultadoColeccion = miLibCol.getResultadosColecciondellibro();
+            JOptionPane.showMessageDialog(null, "La colección del libro es: " + resultadoColeccion);
+
             // Lógica para procesar los datos del libro 
             confirmacion = JOptionPane.showInputDialog("Desea agregar otro libro (si/no): ");
         }
